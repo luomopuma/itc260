8e21622e5f57cbb814811ff38824aa5913afafb5
Sam McHaney <luomopuma@gmail.com>
2016-07-26 17:08:19 -0700

    Adds create functionality.

--- application/models/News_model.php
+++ application/models/News_model.php
@@ -17,4 +17,23 @@ class News_model extends CI_Model {
                 $query = $this->db->get_where('news', array('slug' => $slug));
                 return $query->row_array();
         }
+
+        public function set_news(){
+                // Initialize the URL helper.
+                $this->load->helper('url');
+
+                // CI's "post" function parses the POST superglobal and strips spaces from the title.
+                // URL title changes text into a URL-friendly format, dash separated and lowercase.
+                $slug = url_title($this->input->post('title'),'dash', TRUE);
+
+                // Each array element matches with column titles in the DB.
+                $data = array(
+                        'title' => $this->input->post('title'),
+                        'slug' => $slug,
+                        'text' => $this->input->post('text')
+                );
+
+                // Insert into database.
+                return $this->db->insert('news', $data);
+        }
 }
--- application/views/news/index.php
+++ application/views/news/index.php
@@ -2,14 +2,14 @@
 $this->load->view($this->config->item('theme').'header');
 ?>
 
-<h2><?php $this->config-> ?></h2>
+<h2><?php $title ?></h2>
 
 <?php foreach($news as $news_item): ?>
-<h3><?php echo $news_item['title']; ?></h3>
-<div class = "main">
-    <?php echo $news_item['text'];?>
+    <h3><?php echo $news_item['title']; ?></h3>
+    <div class = "main">
+        <?php echo $news_item['text'];?>
     </div>
     <p><a href="<?php echo site_url('news/'.$news_item['slug']);?>">View article</a></p>
-    <?php endforeach;
-    $this->load->view($this->config->item('theme').'footer');
-    ?>
+<?php endforeach; ?>
+
+<?php $this->load->view($this->config->item('theme').'footer'); ?>
