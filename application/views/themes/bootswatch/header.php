80ad4a807dea8b8f43e80890e5cf626627929170
Sam McHaney <luomopuma@gmail.com>
2016-07-26 18:56:51 -0700

    Fixes default routing issues causing 'index.php' to appear in URL

--- application/views/themes/bootswatch/header.php
+++ application/views/themes/bootswatch/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<!--Bootstrap themes use style settings to change look and feel -->
     <link rel="stylesheet" href="<?=base_url()?>public/themes/bootswatch/css/<?=$this->config->item('style')?>" media="screen">
-	
+
     <link rel="stylesheet" href="<?=base_url()?>public/themes/bootswatch/css/bootswatch.min.css">
     <!--[if lt IE 9]>
 		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
